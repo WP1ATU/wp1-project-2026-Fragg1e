5cd700816cdc6d34e1900f553904f0f7b2889ce3
Fraggle420 <woodwardfraser1@gmail.com>
2026-05-07 13:59:30 +0100

    added basic routes and pages

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
