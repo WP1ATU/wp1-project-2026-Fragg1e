bc19db2bde72368e6992eea1af37f32949e14d0a
Fraggle420 <woodwardfraser1@gmail.com>
2026-05-10 23:29:22 +0100

    update backend to use EC2 endpoints again

--- src/app/services/snooker-api.ts
+++ src/app/services/snooker-api.ts
@@ -10,7 +10,7 @@ import { SnookerEvent } from '../models/event';
   providedIn: 'root'
 })
 export class SnookerApi {
-  private apiUrl = 'http://34.254.151.3:3000/api';
+  private apiUrl = 'http://18.201.114.135:3000/api';
   private playersCache?: Observable<Player[]>;
   private upcomingMatchesCache?: Observable<Match[]>;
 
